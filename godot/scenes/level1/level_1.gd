6e7d2c38647ef88ebcef68b8671eab253828ff3b
simen@bluesmokeelectronics.no <simen@bluesmokeelectronics.no>
2025-01-18 20:12:12 +0100

    added a timer

---
+++ godot/scenes/level1/level_1.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@onready var timer_label: Label = %TimerLabel
+@onready var timer: Timer = $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timer_label.text = str(timer.time_left).pad_decimals(1)
+
+
+func _on_timer_timeout() -> void:
+	get_tree().paused = true

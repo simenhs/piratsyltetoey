6246fc7ff4a2e1e7b3bf3a944e2b6d4ef42fc27d
Tom P. <tompersen@gmail.com>
2025-01-22 20:01:04 +0100

    Added support for looping sounds

--- godot/scripts/globals.gd
+++ godot/scripts/globals.gd
@@ -20,3 +20,25 @@ func play_sound(sound_name: String) -> bool:
 	
 	var success: bool = audio_player.play_sound(sound_name)
 	return success
+
+func play_sound_looping(sound_name: String, key: String) -> bool:
+	if not sound_name:
+		printerr("sound_name was invalid or empty, cannot play sound.")
+		return false
+
+	var audio_player = WORLD_ROOT.get_node("polyphonic_audio_player")
+	if not audio_player:
+		printerr("failed to get audio player node, scene tree likely borked!")
+		return false
+	
+	var success: bool = audio_player.play_sound_looping(sound_name, key)
+	return success
+
+func stop_sound_looping(key: String) -> bool:
+	var audio_player = WORLD_ROOT.get_node("polyphonic_audio_player")
+	if not audio_player:
+		printerr("failed to get audio player node, scene tree likely borked!")
+		return false
+	
+	var success: bool = audio_player.stop_sound_looping(key)
+	return success

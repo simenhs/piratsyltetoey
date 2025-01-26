a43079a53fad313ee3dd3902b3d602f844f84102
Tom P. <tompersen@gmail.com>
2025-01-26 14:03:17 +0100

    Added ingame menu

--- godot/components/menu.gd
+++ godot/components/menu.gd
@@ -1,6 +1,9 @@
 extends Container
 
 var loader = null
+var is_playing := false
+
+signal play_start
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,15 +15,10 @@ func _process(delta: float) -> void:
 	var res = ResourceLoader.load_threaded_get_status(globals.START_SCENE_PATH)
 	if res == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED:
 		var scene_resource = ResourceLoader.load_threaded_get(globals.START_SCENE_PATH)
-		if not scene_resource:
-			print ("FUCKSHIT")
-			get_tree().quit()
 		var scene_instance = scene_resource.instantiate()
-		if not scene_instance:
-			print ("ASSCOCK")
-			get_tree().quit()
 		globals.WORLD_ROOT.get_node("current_level_root").add_child(scene_instance)
 		set_process(false)
+		play_start.emit()
 
 func play_pressed() -> void:
 	loader = ResourceLoader.load_threaded_request(globals.START_SCENE_PATH)
--- godot/scenes/persistent_world/persistent_world.gd
+++ godot/scenes/persistent_world/persistent_world.gd
@@ -1,10 +1,29 @@
 extends Node2D
 
+var is_playing := false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$music_player.volume_db = linear_to_db(globals.DEFAULT_MUSIC_VOLUME)
+	$menu_root/main_menu.play_start.connect(play_start)
+	$menu_root/game_menu.quit_to_main_menu_signal.connect(return_to_menu)
+	$menu_root/game_menu.hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if is_playing:
+		if Input.is_action_just_pressed("open_game_menu"):
+			$menu_root/game_menu.show()
+		else:
+			print("NOT PRESSED")
+	else:
+		print("IS NOT PLAYING")
+
+func play_start() -> void:
+	is_playing = true
+
+func return_to_menu() -> void:
+	is_playing = false
+	for level_scene in $current_level_root.get_children():
+		$current_level_root.remove_child(level_scene)
+		level_scene.queue_free()
+	
+	$menu_root/main_menu.show()
